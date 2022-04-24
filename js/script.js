a9b08fa1c7bcca7f2084b3987b24f55595ade234
NatalieArd <sklyuevan@gmail.com>
2022-04-24 20:03:42 +0300

    MAIN_PROGECT

--- js/script.js
+++ js/script.js
@@ -2,22 +2,6 @@ function myFunction() {
 	document.getElementById("myDropdown").classList.toggle("show");
 }
 
-/*function filterFunction() {
-	var input, filter, ul, li, a, i;
-	input = document.getElementById("myInput");
-	filter = input.value.toUpperCase();
-	div = document.getElementById("myDropdown");
-	a = div.getElementsByTagName("a");
-	for (i = 0; i < a.length; i++) {
-		txtЗначение = a[i].textСодержание || a[i].innerText;
-		if (txtЗначение.toUpperCase().indexOf(filter) > -1) {
-			a[i].style.display = "";
-		} else {
-			a[i].style.display = "none";
-		}
-	}
-} */
-
 var lastResFind = "";
 var copy_page = "";
 
